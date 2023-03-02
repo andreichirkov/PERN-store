c1c87f5a3bf93fc3e49bd7b97c18978693222548
Andrei Chirkov <aachirkov@me.com>
2023-03-02 15:45:40 +0300

    Add styles in auth page

--- client/src/pages/Auth.js
+++ client/src/pages/Auth.js
@@ -1,11 +1,41 @@
-import React from 'react';
+import React from "react"
+import { Button, Card, Container, Form, Row } from "react-bootstrap"
+import { NavLink, useLocation } from "react-router-dom"
+import { LOGIN_ROUTE, REGISTRATION_ROUTE } from "../utils/consts"
 
 const Auth = () => {
-  return (
-    <div>
-      Auth
-    </div>
-  );
-};
+  const location = useLocation()
+  const isLogin = location.pathname === LOGIN_ROUTE
 
-export default Auth;
+  return (
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ height: window.innerHeight - 54 }}>
+      <Card style={{ width: 600 }} className="p-5">
+        <h2 className="m-auto">{isLogin ? "Авторизация" : "Регистрация"}</h2>
+        <Form className="d-flex flex-column">
+          <Form.Control className="mt-3" placeholder="Ведите email" />
+          <Form.Control className="mt-3" placeholder="Ведите пароль" />
+          <Row className="d-flex justify-content-between mt-3 pl-3 pr-3">
+            {isLogin ? (
+              <div>
+                Нет акккаунта? &nbsp;
+                <NavLink to={REGISTRATION_ROUTE}>Зарегистрируйся</NavLink>
+              </div>
+            ) : (
+              <div>
+                Есть аккаунт? &nbsp;
+                <NavLink to={LOGIN_ROUTE}>Войдите</NavLink>
+              </div>
+            )}
+            <Button className=" " variant={"outline-success"}>
+              {isLogin ? 'Войти' : 'Зарегистрироваться'}
+            </Button>
+          </Row>
+        </Form>
+      </Card>
+    </Container>
+  )
+}
+
+export default Auth
