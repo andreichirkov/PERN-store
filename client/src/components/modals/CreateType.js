407e5a780006b0ed42b0769a1e72119cd2911b9f
Andrei Chirkov <aachirkov@me.com>
2023-03-10 15:11:43 +0300

    Create type from UI

--- client/src/components/modals/CreateType.js
+++ client/src/components/modals/CreateType.js
@@ -1,14 +1,21 @@
-import {Button, Form, Modal} from "react-bootstrap";
+import { Button, Form, Modal } from "react-bootstrap"
+import { useState } from "react"
+import { createType } from "../../http/deviceAPI"
 
 //onHide - func которая скрывает модалку
-const CreateType = ({show, onHide}) => {
+const CreateType = ({ show, onHide }) => {
+  const [value, setValue] = useState("")
+
+  const addType = () => {
+    //У создания Типа есть только одно поле name в объекте
+    createType({ name: value }).then(_ => {
+      setValue("")
+      onHide()
+    })
+  }
+
   return (
-    <Modal
-      show={show}
-      onHide={onHide}
-      centered
-      size="lg"
-    >
+    <Modal show={show} onHide={onHide} centered size="lg">
       <Modal.Header>
         <Modal.Title id="contained-modal-title-vcenter">
           Добавить тип
@@ -17,18 +24,21 @@ const CreateType = ({show, onHide}) => {
       <Modal.Body>
         <Form>
           <Form.Control
-            placeholder={'Введите назвние типа'}
-          >
-
-          </Form.Control>
+            placeholder={"Введите назвние типа"}
+            value={value}
+            onChange={e => setValue(e.target.value)}></Form.Control>
         </Form>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant={'outline-danger'} onClick={onHide}>Закрыть</Button>
-        <Button variant={'outline-success'} onClick={onHide}>Добавить</Button>
+        <Button variant={"outline-danger"} onClick={onHide}>
+          Закрыть
+        </Button>
+        <Button variant={"outline-success"} onClick={addType}>
+          Добавить
+        </Button>
       </Modal.Footer>
     </Modal>
-  );
-};
+  )
+}
 
-export default CreateType;
+export default CreateType
